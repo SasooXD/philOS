420fd10a4516d3f6291a01652270ad0766e877a6
Matteo Bertolino <m.bertolino.m@gmail.com>
2025-02-28 22:39:18 +0100

    tools: Update clean.py
    Signed-off-by: Matteo Bertolino <m.bertolino.m@gmail.com>

--- tools/clean.py
+++ tools/clean.py
@@ -1,41 +1,42 @@
 # This is a script that "translates" the output of ia16-elf-gcc -S into something that nasm
-# can understand better. By default, gcc compilation creates assembly files that are then
-# fed into gas which has different directives and other things that we don't need/need to remove.
+# can understand. By default, gcc compilation creates assembly files that are then fed
+# into gas which has different directives and other things that we don't need/need to remove.
 # The -masm=intel flag from ia16-elf-gcc is still needed tho, because not a chance i'm
 # reading/writing assembly with that absolutely horrendous and complete dogshit of at&t syntax.
-
-#TODO: rewrite this (kinda works for our purposes but needs to be translated and reformatted)
 
 import re
 import sys
 
 def clean_assembly(input_file):
-    with open(input_file, 'r') as infile:
-        lines = infile.readlines()
+	with open(input_file, 'r') as infile:
+		lines = infile.readlines()
 
-    cleaned_lines = []
-    for line in lines:
-        line = re.sub(r'^[ ]+', '\t', line)
+	cleaned_lines = []
+	for line in lines:
+		line = re.sub(r'^[ ]+', '\t', line) # Tabs!!!
 
-        if any(directive in line for directive in [
-            ".arch", ".code16", ".intel_syntax", "#NO_APP",
-            ".section", ".global", ".type", ".size", ".ident"
-        ]):
-            continue
+		# gas keywords we don't want
+		if any(directive in line for directive in [
+			".arch", ".code16", ".intel_syntax", "#NO_APP",
+			".section", ".global", ".type", ".size", ".ident"
+		]):
+			continue
 
-        line = re.sub(r'\b([a-z]+)\b', lambda m: m.group(1).upper(), line)
-        line = re.sub(r'\t+', ' ', line)
-        line = re.sub(r'\bMAIN\b', 'start', line)
+		# Spooky regex magic
+		line = re.sub(r'\b([a-z]+)\b', lambda m: m.group(1).upper(), line)
+		line = re.sub(r'\t+', ' ', line)
+		line = re.sub(r'\bMAIN\b', 'start', line)
 
-        if line.strip():
-            cleaned_lines.append(line)
+		if line.strip():
+			cleaned_lines.append(line)
 
-    with open(input_file, 'w', newline='\n') as outfile:
-        outfile.writelines(cleaned_lines)
+	with open(input_file, 'w', newline='\n') as outfile:
+		outfile.writelines(cleaned_lines)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Uso: {sys.argv[0]} <file_input>")
-        sys.exit(1)
+	# Nuke everything if there are not exactly two arguments
+	if len(sys.argv) != 2:
+		print(f"Usage: {sys.argv[0]} <input_file>.")
+		sys.exit(1)
 
-    clean_assembly(sys.argv[1])
+	clean_assembly(sys.argv[1])
